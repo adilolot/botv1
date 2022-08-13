fd3aaf8e10c2156a8e52780f29004bf8d0455d76
adilolot <103311965+adilolot@users.noreply.github.com>
2022-08-13 23:38:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['6281511480762'],
-  ['6281511480762', 'YahyaGanzz', true]
+  ['6285766721335'],
+  ['6285766721335', 'Adi setore', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -59,7 +59,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = 'AzzBot-Yahya'
+global.wm = 'AdiBot-Adi setore'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
